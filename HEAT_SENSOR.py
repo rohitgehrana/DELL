9751999664d6c063e985f17be64c720d5e3532c6
rohitgehrana <151773421+rohitgehrana@users.noreply.github.com>
2023-12-02 07:40:44 +0000

    Update HEAT_SENSOR.py

--- HEAT_SENSOR.py
+++ HEAT_SENSOR.py
@@ -1,1 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+# Set the GPIO mode and pin
+sensor_pin = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensor_pin, GPIO.IN)
+
+try:
+    while True:
+        # Read the state of the sensor
+        sensor_state = GPIO.input(sensor_pin)
+
+        # Check if the sensor is detected (modify this condition based on your sensor)
+        if sensor_state == GPIO.HIGH:
+            print("SENSOR DETECTED")
+
+        # Add a small delay to avoid high CPU usage
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    # Clean up GPIO on program exit
+    GPIO.cleanup()
 
